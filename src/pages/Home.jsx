585d1293638028541061570336a5103024ba90a4
zubayr-parker <zubayr_parker@outlook.com>
2023-08-21 15:32:00 +1000

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react'
+import { Axios } from 'axios'
+
+function Home() { return(<></>)
+
+
+//     useEffect(()=>{
+//       console.log(await axios.get("https://jsonplaceholder.typicode.com/users"))
+//     },[])
+
+
+//   return (
+//     <div className="container">
+//   <div className="row">
+//     <div className="user-list">
+//       <div className="user">
+//         <div className="user-card">
+//           <div className="user-card__container">
+//             <h3>{user.name}</h3>
+//             <p>
+//               <b>Email:</b> {user.email}
+//             </p>
+//             <p>
+//               <b>Phone:</b> {user.phone}
+//             </p>
+//             <p>
+//               <b>Website:</b>
+//               {user.website}
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+  
+}
+
+export default Home
