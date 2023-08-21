1dfe2cf17377c56bd6573a16933d4bd86c213c6f
zubayr-parker <zubayr_parker@outlook.com>
2023-08-21 16:30:06 +1000

    everything is working!

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,50 @@
-import React, { useEffect } from 'react'
-import { Axios } from 'axios'
-
-function Home() { return(<></>)
-
-
-//     useEffect(()=>{
-//       console.log(await axios.get("https://jsonplaceholder.typicode.com/users"))
-//     },[])
+import React, { useEffect, useState } from "react";
+import axios, { Axios } from "axios";
+import { useNavigate } from "react-router-dom";
 
 
-//   return (
-//     <div className="container">
-//   <div className="row">
-//     <div className="user-list">
-//       <div className="user">
-//         <div className="user-card">
-//           <div className="user-card__container">
-//             <h3>{user.name}</h3>
-//             <p>
-//               <b>Email:</b> {user.email}
-//             </p>
-//             <p>
-//               <b>Phone:</b> {user.phone}
-//             </p>
-//             <p>
-//               <b>Website:</b>
-//               {user.website}
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// </div>
-  
+function Home() {
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate()
+
+  async function getUsers() {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="user-list">
+          {users.map((user, index) => (
+            <div className="user" key={user.id} onClick={()=> navigate(`${user.id}`)}>
+              <div className="user-card">
+                <div className="user-card__container">
+                  <h3>{user.name}</h3>
+                  <p>
+                    <b>Email:</b> {user.email}
+                  </p>
+                  <p>
+                    <b>Phone:</b> {user.phone}
+                  </p>
+                  <p>
+                    <b>Website:</b>
+                    {user.website}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
